35ab76bebdac37a0136633d9beaceebd8a7604a5
Yongmin0423 <yym21345@gmail.com>
2024-10-31 01:06:30 +0900

    cart delete and cart amount

--- controller/cartController.js
+++ controller/cartController.js
@@ -30,3 +30,66 @@ export const addItemToCart = async (req, res) => {
     return res.status(400).json({ status: "fail", error: error.message });
   }
 };
+
+export const getCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: { path: "productId", model: "Product" },
+    });
+    return res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+export const deleteCart = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId });
+    cart.items = cart.items.filter((item) => !item._id.equals(id));
+    await cart.save();
+    return res
+      .status(200)
+      .json({ status: "success", data: cart, cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+export const editCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+
+    const { qty } = req.body;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    if (!cart) throw new Error("There is no cart for this user");
+    const index = cart.items.findIndex((item) => item._id.equals(id));
+    if (index === -1) throw new Error("Can not find item");
+    cart.items[index].qty = qty;
+    await cart.save();
+    return res.status(200).json({ status: 200, data: cart.items });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+export const getCartQty = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId });
+    if (!cart) throw new Error("There is no cart!");
+    return res.status(200).json({ status: "success", qty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,9 +1,22 @@
 import express from "express";
 import { authenticate } from "../controller/authController.js";
 import { addItemToCart } from "../controller/cartController.js";
+import { getCart } from "../controller/cartController.js";
+import { deleteCart } from "../controller/cartController.js";
+import { editCart } from "../controller/cartController.js";
+import { getCartQty } from "../controller/cartController.js";
 
 const cartRouter = express.Router();
 
-cartRouter.route("/").post(authenticate, addItemToCart);
+cartRouter
+  .route("/")
+  .post(authenticate, addItemToCart)
+  .get(authenticate, getCart);
 
+cartRouter
+  .route("/:id")
+  .delete(authenticate, deleteCart)
+  .put(authenticate, editCart);
+
+cartRouter.get("/qty", authenticate, getCartQty);
 export default cartRouter;
